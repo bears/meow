e49ec075203cc1378a88b6f8b7105421b22a7f85
Iceberg Young <iceberg.young@gmail.com>
2014-07-20 12:18:38 +0800

    Modified checklist.

--- public/js/flow.js
+++ public/js/flow.js
@@ -9,5 +9,26 @@
 			$scope.color = color;
 		};
 		$scope.title = 'こんな何气ない一日新';
+		$scope.checklists = [
+			{
+				title : 'Some pairs',
+				list : [
+					{title : 'min-max pair.', checked : false},
+					{title : 'key-value pair.', checked : true}
+				]
+			},
+			{
+				title : 'Unnecessary list',
+				list : [
+					{title : 'append|insert into [][]...', checked : false}
+				]
+			}
+		];
+		$scope.checkLineClass = function (checked) {
+			return checked ? 'text-muted' : 'text-' + $scope.color;
+		};
+		$scope.checkLineIcon = function(checked) {
+			return checked ? 'glyphicon-check' : 'glyphicon-unchecked';
+		};
 	});
 })(angular.module('meow'));
